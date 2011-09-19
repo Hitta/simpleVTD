51c89c227cf56d3c4636db7f9b8df8b5916fe09e
matsbov <mats.bovin@gmail.com>
2011-09-19 11:10:41 +0000

    Updated poms Housekeeping after dependency changes (commons.lang3)
    git-svn-id: http://scm.hitta.local/subversion/utveckling/services/Hitta.SimpleVTD/trunk@9180 4752078c-5cf1-a944-ae95-26af0396db54

--- src/test/java/se/hitta/simplevtd/mappers/MappersTest.java
+++ src/test/java/se/hitta/simplevtd/mappers/MappersTest.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
